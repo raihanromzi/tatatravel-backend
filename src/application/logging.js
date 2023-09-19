8f42c0e5a21b795f84674fbe7c883ff7f63ccc4f
raihanromzi <raihan.rakhman16@gmail.com>
2023-09-19 13:24:37 +0700

    setup project

---
+++ src/application/logging.js
@@ -0,0 +1,10 @@
+// winston logger
+import winston from "winston";
+
+export const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console({})
+    ]
+})
---
+++ src/application/web.js
@@ -0,0 +1,4 @@
+import express from "express"
+
+const web = express()
+web.use(express.json())
