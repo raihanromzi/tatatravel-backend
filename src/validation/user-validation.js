ebc863d59893e5e40193b104ddd1bacf9d5dd06c
raihanromzi <raihan.rakhman16@gmail.com>
2023-09-23 16:44:44 +0700

    update validation with constant message

--- src/validation/user-validation.js
+++ src/validation/user-validation.js
@@ -1,57 +1,101 @@
 import Joi from 'joi'
+import { errors } from '../utils/message-error.js'
 
 const addUserValidationSchema = Joi.object({
-    firstName: Joi.string().min(3).max(50).required().messages({
-        'string.base': 'first name must be a string',
-        'string.empty': 'first name cannot be empty',
-        'string.min': 'min 3 characters',
-        'string.max': 'max 50 characters',
-        'any.required': 'first name is required!',
-    }),
-    lastName: Joi.string().min(3).max(50).required().messages({
-        'string.base': 'last name must be a string',
-        'string.empty': 'last name cannot be empty',
-        'string.min': 'min 3 characters',
-        'string.max': 'max 50 characters',
-        'any.required': 'last name is required!',
-    }),
-    username: Joi.string().min(3).max(30).required().messages({
-        'string.base': 'username must be a string',
-        'string.empty': 'username cannot be empty',
-        'string.min': 'min 3 characters',
-        'string.max': 'max 30 characters',
-        'any.required': 'username is required!',
-    }),
-    email: Joi.string().email().required().messages({
-        'string.base': 'email must be a string',
-        'string.empty': 'email cannot be empty',
-        'string.email': 'email must be a valid email',
-        'any.required': 'email is required!',
-    }),
-    password: Joi.string().min(6).max(255).required().messages({
-        'string.base': 'password must be a string',
-        'string.empty': 'password cannot be empty',
-        'string.min': 'min 6 characters',
-        'string.max': 'max 255 characters',
-        'any.only': 'Password is required!',
-    }),
-    role: Joi.number().required(),
+    firstName: Joi.string()
+        .min(3)
+        .max(50)
+        .required()
+        .messages({
+            'string.base': `${errors.ERROR_FIRST_NAME_STRING}`,
+            'string.empty': `${errors.ERROR_FIRST_NAME_EMPTY}`,
+            'string.min': `${errors.ERROR_FIRST_NAME_MIN}`,
+            'string.max': `${errors.ERROR_FIRST_NAME_MAX}`,
+            'any.required': `${errors.ERROR_FIRSTNAME_REQUIRED}`,
+        }),
+    lastName: Joi.string()
+        .min(3)
+        .max(50)
+        .required()
+        .messages({
+            'string.base': `${errors.ERROR_LAST_NAME_STRING}`,
+            'string.empty': `${errors.ERROR_LAST_NAME_EMPTY}`,
+            'string.min': `${errors.ERROR_LAST_NAME_MIN}`,
+            'string.max': `${errors.ERROR_LAST_NAME_MAX}`,
+            'any.required': `${errors.ERROR_LASTNAME_REQUIRED}`,
+        }),
+    username: Joi.string()
+        .min(3)
+        .max(30)
+        .required()
+        .messages({
+            'string.base': `${errors.ERROR_USERNAME_STRING}`,
+            'string.empty': `${errors.ERROR_USERNAME_EMPTY}`,
+            'string.min': `${errors.ERROR_USERNAME_MIN}`,
+            'string.max': `${errors.ERROR_USERNAME_MAX}`,
+            'any.required': `${errors.ERROR_USERNAME_REQUIRED}`,
+        }),
+    email: Joi.string()
+        .email()
+        .required()
+        .messages({
+            'string.base': `${errors.ERROR_EMAIL_STRING}`,
+            'string.empty': `${errors.ERROR_EMAIL_EMPTY}`,
+            'string.email': `${errors.ERROR_EMAIL_INVALID}`,
+            'any.required': `${errors.ERROR_EMAIL_REQUIRED}`,
+        }),
+    password: Joi.string()
+        .min(6)
+        .max(255)
+        .required()
+        .pattern(new RegExp('^[a-zA-Z0-9]{6,255}$'))
+        .messages({
+            'string.base': `${errors.ERROR_PASSWORD_STRING}`,
+            'string.empty': `${errors.ERROR_PASSWORD_EMPTY}`,
+            'string.min': `${errors.ERROR_PASSWORD_MIN}`,
+            'string.max': `${errors.ERROR_PASSWORD_MAX}`,
+            'any.required': `${errors.ERROR_PASSWORD_REQUIRED}`,
+        }),
+    role: Joi.number()
+        .required()
+        .positive()
+        .messages({
+            'number.base': `${errors.ERROR_ROLE_NUMBER}`,
+            'number.positive': `${errors.ERROR_ROLE_NUMBER}`,
+        }),
 }).unknown(true)
 
 const loginValidationSchema = Joi.object({
-    email: Joi.string().email().required().messages({
-        'string.base': 'email must be a string',
-        'string.empty': 'email cannot be empty',
-        'string.email': 'email must be a valid email',
-        'any.required': 'email is required!',
-    }),
-    password: Joi.string().min(6).max(255).required().messages({
-        'string.base': 'password must be a string',
-        'string.empty': 'password cannot be empty',
-        'string.min': 'min 6 characters',
-        'string.max': 'max 255 characters',
-        'any.only': 'Password is required!',
-    }),
+    email_username: [
+        Joi.string()
+            .email()
+            .messages({
+                'string.base': `${errors.ERROR_EMAIL_STRING}`,
+                'string.empty': `${errors.ERROR_EMAIL_EMPTY}`,
+                'string.email': `${errors.ERROR_EMAIL_INVALID}`,
+            }),
+        Joi.string()
+            .min(3)
+            .max(30)
+            .messages({
+                'string.base': `${errors.ERROR_USERNAME_STRING}`,
+                'string.empty': `${errors.ERROR_USERNAME_EMPTY}`,
+                'string.min': `${errors.ERROR_USERNAME_MIN}`,
+                'string.max': `${errors.ERROR_USERNAME_MAX}`,
+            }),
+    ],
+    password: Joi.string()
+        .min(6)
+        .max(255)
+        .required()
+        .pattern(new RegExp('^[a-zA-Z0-9]{6,255}$'))
+        .messages({
+            'string.base': `${errors.ERROR_PASSWORD_STRING}`,
+            'string.empty': `${errors.ERROR_PASSWORD_EMPTY}`,
+            'string.min': `${errors.ERROR_PASSWORD_MIN}`,
+            'string.max': `${errors.ERROR_PASSWORD_MAX}`,
+            'any.required': `${errors.ERROR_PASSWORD_REQUIRED}`,
+        }),
 }).unknown(true)
 
 const getUserValidationSchema = Joi.string().min(3).max(30).required().messages({
