b6b19204555301f1b4e5c30d2c345af73110f818
raihanromzi <raihan.rakhman16@gmail.com>
2023-09-28 12:30:10 +0700

    add refresh token check middleware

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import userController from '../controllers/user-controller.js'
-import { accessTokenVerifyMiddleware } from '../middlewares/token-middleware.js'
+import {
+    accessTokenVerifyMiddleware,
+    refreshTokenVerifyMiddleware,
+} from '../middlewares/token-middleware.js'
 
 const userRouter = express.Router()
 
 userRouter.use(accessTokenVerifyMiddleware)
+userRouter.use(refreshTokenVerifyMiddleware)
 
 userRouter.get('/api/v1/users/current', userController.get)
 userRouter.patch('/api/v1/users/current', userController.update)
