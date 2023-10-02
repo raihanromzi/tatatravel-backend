2d5483b79974071c449c7585b756025208a9771b
raihanromzi <raihan.rakhman16@gmail.com>
2023-10-02 08:45:34 +0700

    add error middleware so it can handle mutler size limit

--- src/middlewares/error-middleware.js
+++ src/middlewares/error-middleware.js
@@ -12,6 +12,17 @@ const errorMiddleware = async (err, req, res, next) => {
         res.status(err.code)
             .send(response.responseError(err.code, err.status, err.message))
             .end()
+    } else if (err.code === 'LIMIT_FILE_SIZE') {
+        // handle multer limit file size
+        res.status(errors.HTTP.CODE.BAD_REQUEST)
+            .send(
+                response.responseError(
+                    errors.HTTP.CODE.BAD_REQUEST,
+                    errors.HTTP.STATUS.BAD_REQUEST,
+                    errors.AVATAR.MUST_LESS_THAN_2MB
+                )
+            )
+            .end()
     } else {
         res.status(errors.HTTP.CODE.INTERNAL_SERVER_ERROR)
             .send(
