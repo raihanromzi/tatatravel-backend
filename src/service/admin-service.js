0a36cf8aadfc9f1abbf9aeeb7ca2ada9bad5b450
raihanromzi <raihan.rakhman16@gmail.com>
2023-10-04 05:32:42 +0700

    refactoring admin api

--- src/service/admin-service.js
+++ src/service/admin-service.js
@@ -17,25 +17,43 @@ const add = async (req) => {
 
     const { fullName, username, email, password, role } = user
 
-    const [, countUser, result] = await prismaClient.$transaction([
-        prismaClient.role.findFirstOrThrow({
+    return prismaClient.$transaction(async (prisma) => {
+        const findRole = await prisma.role.findUnique({
             where: {
                 id: role,
             },
-        }),
-        prismaClient.user.count({
+        })
+
+        if (!findRole) {
+            throw new ResponseError(
+                errors.HTTP.CODE.NOT_FOUND,
+                errors.HTTP.STATUS.NOT_FOUND,
+                errors.ROLE.NOT_FOUND
+            )
+        }
+
+        const countSameUsernameOrEmail = await prisma.user.count({
             where: {
                 OR: [
                     {
-                        email: user.email,
+                        email: email,
                     },
                     {
-                        username: user.username,
+                        username: username,
                     },
                 ],
             },
-        }),
-        prismaClient.user.create({
+        })
+
+        if (countSameUsernameOrEmail === 1) {
+            throw new ResponseError(
+                errors.HTTP.CODE.BAD_REQUEST,
+                errors.HTTP.STATUS.BAD_REQUEST,
+                errors.USER.ALREADY_EXISTS
+            )
+        }
+
+        const result = await prisma.user.create({
             data: {
                 fullName: fullName,
                 username: username,
@@ -51,33 +69,25 @@ const add = async (req) => {
                 username: true,
                 email: true,
             },
-        }),
-    ])
+        })
 
-    if (countUser === 1) {
-        throw new ResponseError(
-            errors.HTTP.CODE.BAD_REQUEST,
-            errors.HTTP.STATUS.BAD_REQUEST,
-            errors.USER.ALREADY_EXISTS
-        )
-    }
+        if (!result) {
+            throw new ResponseError(
+                errors.HTTP.CODE.INTERNAL_SERVER_ERROR,
+                errors.HTTP.STATUS.INTERNAL_SERVER_ERROR,
+                errors.USER.FAILED_TO_ADD
+            )
+        }
 
-    if (!result) {
-        throw new ResponseError(
-            errors.HTTP.CODE.INTERNAL_SERVER_ERROR,
-            errors.HTTP.STATUS.INTERNAL_SERVER_ERROR,
-            errors.USER.FAILED_TO_ADD
-        )
-    }
-
-    return result
+        return result
+    })
 }
 const remove = async (req) => {
-    const id = validate(deleteUserValidationSchema, req.params.id)
+    const paramUserId = validate(deleteUserValidationSchema, req.params.id)
 
-    const currentUserId = req.user.id
+    const { id: currentUserId } = req.user
 
-    if (id === currentUserId) {
+    if (paramUserId === currentUserId) {
         throw new ResponseError(
             errors.HTTP.CODE.FORBIDDEN,
             errors.HTTP.STATUS.FORBIDDEN,
@@ -85,36 +95,35 @@ const remove = async (req) => {
         )
     }
 
-    const [foundUser, deletedUser] = await prismaClient.$transaction([
-        prismaClient.user.count({
+    return prismaClient.$transaction(async (prisma) => {
+        const findUser = await prisma.user.findUnique({
             where: {
-                id: id,
+                id: paramUserId,
             },
-        }),
-        prismaClient.user.delete({
+        })
+
+        if (!findUser) {
+            throw new ResponseError(
+                errors.HTTP.CODE.NOT_FOUND,
+                errors.HTTP.STATUS.NOT_FOUND,
+                errors.USER.NOT_FOUND
+            )
+        }
+
+        fs.rmSync(`public/images/avatar/${paramUserId}`, { recursive: true, force: true })
+
+        return prisma.user.delete({
             where: {
-                id: id,
+                id: paramUserId,
             },
-        }),
-    ])
-
-    if (!foundUser) {
-        throw new ResponseError(
-            errors.HTTP.CODE.NOT_FOUND,
-            errors.HTTP.STATUS.NOT_FOUND,
-            errors.USER.NOT_FOUND
-        )
-    }
-
-    fs.rmSync(`public/images/avatar/${id}`, { recursive: true, force: true })
-
-    return deletedUser
+        })
+    })
 }
 
 const get = async (req) => {
     const query = validate(searchUserValidationSchema, req.query)
 
-    const id = req.user.id
+    const userId = req.user.id
 
     const { name, email, username, role, page, size, sortBy, orderBy } = query
 
@@ -173,18 +182,12 @@ const get = async (req) => {
         })
     }
 
-    if (sortBy && orderBy) {
-        filters.push({
-            [sortBy]: orderBy,
-        })
-    }
-
-    const [users, totalItems] = await prismaClient.$transaction([
-        prismaClient.user.findMany({
+    return prismaClient.$transaction(async (prisma) => {
+        const users = await prisma.user.findMany({
             where: {
                 AND: filters,
                 NOT: {
-                    id: id,
+                    id: userId,
                 },
             },
             select: {
@@ -200,35 +203,39 @@ const get = async (req) => {
             },
             take: size,
             skip: skip,
-        }),
-        prismaClient.user.count({
+            orderBy: {
+                [sortBy]: orderBy,
+            },
+        })
+
+        // convert role object to role name
+        users.forEach((user) => {
+            user.role = user.role.name
+        })
+
+        const totalItems = await prisma.user.count({
             where: {
                 AND: filters,
             },
-        }),
-    ])
+        })
 
-    // convert role object to role name
-    users.forEach((user) => {
-        user.role = user.role.name
+        return {
+            data: users,
+            pagination: {
+                page: page,
+                total_item: totalItems,
+                total_page: Math.ceil(totalItems / size),
+            },
+        }
     })
-
-    return {
-        data: users,
-        pagination: {
-            page: page,
-            total_item: totalItems,
-            total_page: Math.ceil(totalItems / size),
-        },
-    }
 }
 
 const update = async (req) => {
-    const id = validate(getUserValidationSchema, req.params)
+    const paramUserid = validate(getUserValidationSchema, req.params)
 
     const currentUserId = req.user.id
 
-    if (id === currentUserId) {
+    if (paramUserid === currentUserId) {
         throw new ResponseError(
             errors.HTTP.CODE.FORBIDDEN,
             errors.HTTP.STATUS.FORBIDDEN,
@@ -240,39 +247,40 @@ const update = async (req) => {
 
     const { isActive } = user
 
-    const [countUser, result] = await prismaClient.$transaction([
-        prismaClient.user.count({
+    return prismaClient.$transaction(async (prisma) => {
+        const findUser = await prisma.user.findUnique({
             where: {
-                id: id,
+                id: paramUserid,
             },
-        }),
-        prismaClient.user.update({
+        })
+
+        if (!findUser) {
+            throw new ResponseError(
+                errors.HTTP.CODE.NOT_FOUND,
+                errors.HTTP.STATUS.NOT_FOUND,
+                errors.USER.NOT_FOUND
+            )
+        }
+
+        const result = prisma.user.update({
             where: {
-                id: id,
+                id: paramUserid,
             },
             data: {
                 isActive: isActive,
             },
-        }),
-    ])
+        })
 
-    if (countUser !== 1) {
-        throw new ResponseError(
-            errors.HTTP.CODE.NOT_FOUND,
-            errors.HTTP.STATUS.NOT_FOUND,
-            errors.USER.NOT_FOUND
-        )
-    }
+        if (!result) {
+            throw new ResponseError(
+                errors.HTTP.CODE.INTERNAL_SERVER_ERROR,
+                errors.HTTP.STATUS.INTERNAL_SERVER_ERROR,
+                errors.USER.FAILED_TO_UPDATE
+            )
+        }
 
-    if (!result) {
-        throw new ResponseError(
-            errors.HTTP.CODE.INTERNAL_SERVER_ERROR,
-            errors.HTTP.STATUS.INTERNAL_SERVER_ERROR,
-            errors.USER.FAILED_TO_UPDATE
-        )
-    }
-
-    return result
+        return result
+    })
 }
 
 export default { add, remove, get, update }
