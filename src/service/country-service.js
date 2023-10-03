2a5735328158995da4b165f7eb6e95a24755813e
raihanromzi <raihan.rakhman16@gmail.com>
2023-10-04 00:08:53 +0700

    refactoring country api

--- src/service/country-service.js
+++ src/service/country-service.js
@@ -13,41 +13,44 @@ import { errors } from '../utils/message-error.js'
 const add = async (req) => {
     const country = validate(addCountryValidationSchema, req.body)
 
-    const [countCountry, result] = await prismaClient.$transaction([
-        prismaClient.country.count({
+    const { name, areaId } = country
+
+    return prismaClient.$transaction(async (prisma) => {
+        const countCountry = await prisma.country.count({
             where: {
-                name: country.name,
+                name: name,
             },
-        }),
-        prismaClient.country.create({
+        })
+
+        if (countCountry === 1) {
+            throw new ResponseError(
+                errors.HTTP.CODE.BAD_REQUEST,
+                errors.HTTP.STATUS.BAD_REQUEST,
+                errors.COUNTRY.ALREADY_EXISTS
+            )
+        }
+
+        const result = await prisma.country.create({
             data: {
-                name: country.name,
-                areaId: country.areaId,
+                name: name,
+                areaId: areaId,
             },
             select: {
                 id: true,
                 name: true,
             },
-        }),
-    ])
+        })
 
-    if (countCountry === 1) {
-        throw new ResponseError(
-            errors.HTTP.CODE.BAD_REQUEST,
-            errors.HTTP.STATUS.BAD_REQUEST,
-            errors.COUNTRY.ALREADY_EXISTS
-        )
-    }
+        if (!result) {
+            throw new ResponseError(
+                errors.HTTP.CODE.INTERNAL_SERVER_ERROR,
+                errors.HTTP.STATUS.INTERNAL_SERVER_ERROR,
+                errors.COUNTRY.FAILED_TO_ADD
+            )
+        }
 
-    if (!result) {
-        throw new ResponseError(
-            errors.HTTP.CODE.INTERNAL_SERVER_ERROR,
-            errors.HTTP.STATUS.INTERNAL_SERVER_ERROR,
-            errors.COUNTRY.FAILED_TO_ADD
-        )
-    }
-
-    return result
+        return result
+    })
 }
 
 const update = async (req) => {
@@ -57,20 +60,38 @@ const update = async (req) => {
 
     const { name, areaId } = country
 
-    const countryId = params.id
+    const { id: countryId } = params
 
-    const [, , result] = await prismaClient.$transaction([
-        prismaClient.country.findUniqueOrThrow({
+    return prismaClient.$transaction(async (prisma) => {
+        const findCountry = await prisma.country.findUnique({
             where: {
                 id: countryId,
             },
-        }),
-        prismaClient.area.findUniqueOrThrow({
+        })
+
+        if (!findCountry) {
+            throw new ResponseError(
+                errors.HTTP.CODE.NOT_FOUND,
+                errors.HTTP.STATUS.NOT_FOUND,
+                errors.COUNTRY.NOT_FOUND
+            )
+        }
+
+        const findArea = await prisma.area.findUnique({
             where: {
                 id: areaId,
             },
-        }),
-        prismaClient.country.update({
+        })
+
+        if (!findArea) {
+            throw new ResponseError(
+                errors.HTTP.CODE.NOT_FOUND,
+                errors.HTTP.STATUS.NOT_FOUND,
+                errors.AREA.NOT_FOUND
+            )
+        }
+
+        const result = await prisma.country.update({
             where: {
                 id: countryId,
             },
@@ -87,47 +108,56 @@ const update = async (req) => {
                     },
                 },
             },
-        }),
-    ])
+        })
 
-    if (!result) {
-        throw new ResponseError(
-            errors.HTTP.CODE.INTERNAL_SERVER_ERROR,
-            errors.HTTP.STATUS.INTERNAL_SERVER_ERROR,
-            errors.COUNTRY.FAILED_TO_UPDATE
-        )
-    }
+        if (!result) {
+            throw new ResponseError(
+                errors.HTTP.CODE.INTERNAL_SERVER_ERROR,
+                errors.HTTP.STATUS.INTERNAL_SERVER_ERROR,
+                errors.COUNTRY.FAILED_TO_UPDATE
+            )
+        }
 
-    return result
+        return result
+    })
 }
 
 const remove = async (req) => {
     const country = validate(deleteCountryValidationSchema, req.params)
 
-    const countryId = country.id
+    const { id: countryId } = country
 
-    const [, result] = await prismaClient.$transaction([
-        prismaClient.country.findUniqueOrThrow({
+    return prismaClient.$transaction(async (prisma) => {
+        const findCountry = await prisma.country.findUnique({
             where: {
                 id: countryId,
             },
-        }),
-        prismaClient.country.delete({
+        })
+
+        if (!findCountry) {
+            throw new ResponseError(
+                errors.HTTP.CODE.NOT_FOUND,
+                errors.HTTP.STATUS.NOT_FOUND,
+                errors.COUNTRY.NOT_FOUND
+            )
+        }
+
+        const result = await prisma.country.delete({
             where: {
                 id: countryId,
             },
-        }),
-    ])
+        })
 
-    if (!result) {
-        throw new ResponseError(
-            errors.HTTP.CODE.INTERNAL_SERVER_ERROR,
-            errors.HTTP.STATUS.INTERNAL_SERVER_ERROR,
-            errors.COUNTRY.FAILED_TO_DELETE
-        )
-    }
+        if (!result) {
+            throw new ResponseError(
+                errors.HTTP.CODE.INTERNAL_SERVER_ERROR,
+                errors.HTTP.STATUS.INTERNAL_SERVER_ERROR,
+                errors.COUNTRY.FAILED_TO_DELETE
+            )
+        }
 
-    return result
+        return result
+    })
 }
 
 const get = async (req) => {
@@ -139,7 +169,7 @@ const get = async (req) => {
 
     const filters = []
 
-    if (query.name) {
+    if (name) {
         filters.push({
             name: {
                 contains: name,
@@ -147,8 +177,8 @@ const get = async (req) => {
         })
     }
 
-    const [countries, totalItems] = await prismaClient.$transaction([
-        prismaClient.country.findMany({
+    return prismaClient.$transaction(async (prisma) => {
+        const countries = await prisma.country.findMany({
             where: {
                 AND: filters,
             },
@@ -164,63 +194,74 @@ const get = async (req) => {
             },
             take: size,
             skip: skip,
-        }),
-        prismaClient.country.count({
+        })
+
+        const totalItems = await prisma.country.count({
             where: {
                 AND: filters,
             },
-        }),
-    ])
+        })
 
-    const result = countries.map((country) => {
-        const { id, name, area } = country
+        const result = countries.map((country) => {
+            const { id, name, area } = country
+
+            return {
+                id: id,
+                name: name,
+                areaId: area.id,
+                area: area.name,
+            }
+        })
 
         return {
-            id: id,
-            name: name,
-            areaId: area.id,
-            area: area.name,
+            data: result,
+            pagination: {
+                page: page,
+                total_item: totalItems,
+                total_page: Math.ceil(totalItems / size),
+            },
         }
     })
-
-    return {
-        data: result,
-        pagination: {
-            page: page,
-            total_item: totalItems,
-            total_page: Math.ceil(totalItems / size),
-        },
-    }
 }
 
 const getById = async (req) => {
     const params = validate(getCountryByIdValidationSchema, req.params)
 
-    const id = params.id
+    const { id } = params
 
-    const country = await prismaClient.country.findUniqueOrThrow({
-        where: {
-            id: id,
-        },
-        select: {
-            id: true,
-            name: true,
-            area: {
-                select: {
-                    id: true,
-                    name: true,
+    return prismaClient.$transaction(async (prisma) => {
+        const country = await prisma.country.findUnique({
+            where: {
+                id: id,
+            },
+            select: {
+                id: true,
+                name: true,
+                area: {
+                    select: {
+                        id: true,
+                        name: true,
+                    },
                 },
             },
-        },
+        })
+
+        if (!country) {
+            throw new ResponseError(
+                errors.HTTP.CODE.NOT_FOUND,
+                errors.HTTP.STATUS.NOT_FOUND,
+                errors.COUNTRY.NOT_FOUND
+            )
+        }
+
+        const { countryId, name, area } = country
+
+        return {
+            id: countryId,
+            name: name,
+            areaId: area.id,
+            area: area.name,
+        }
     })
-
-    const { countryId, name, area } = country
-
-    return {
-        id: countryId,
-        name: name,
-        areaId: area.id,
-        area: area.name,
-    }
 }
 export default { add, get, getById, update, remove }
