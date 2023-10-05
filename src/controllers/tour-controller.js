83a2896062e8028d98729c5c0615c06e34dcf481
raihanromzi <raihan.rakhman16@gmail.com>
2023-10-05 17:16:46 +0700

    remove unused tour controller

--- src/controllers/tour-controller.js
+++ src/controllers/tour-controller.js
@@ -1,37 +1,17 @@
 import responses from '../utils/response-api.js'
 import tourService from '../service/tour-service.js'
+import { success } from '../utils/message-success.js'
 
 const add = async (req, res, next) => {
     try {
         const result = await tourService.add(req)
-        res.status(201).send(responses.responseSuccess(201, 'CREATED', result))
-    } catch (e) {
-        next(e)
-    }
-}
-
-const update = async (req, res, next) => {
-    try {
-        const result = await tourService.update(req)
-        res.status(200).send(responses.responseSuccess(200, 'OK', result))
-    } catch (e) {
-        next(e)
-    }
-}
-
-const get = async (req, res, next) => {
-    try {
-        const result = await tourService.get()
-        res.status(200).send(responses.responseSuccess(200, 'OK', result))
-    } catch (e) {
-        next(e)
-    }
-}
-
-const remove = async (req, res, next) => {
-    try {
-        const result = await tourService.remove(req)
-        res.status(200).send(responses.responseSuccess(200, 'OK', result))
+        res.status(success.HTTP.CODE.CREATED).send(
+            responses.responseSuccess(
+                success.HTTP.CODE.CREATED,
+                success.HTTP.STATUS.CREATED,
+                result
+            )
+        )
     } catch (e) {
         next(e)
     }
@@ -39,7 +19,4 @@ const remove = async (req, res, next) => {
 
 export default {
     add,
-    update,
-    get,
-    remove,
 }
