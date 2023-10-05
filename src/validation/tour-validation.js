0838551ce5346aea6eb80e3f2ec3c4aa718c1a9b
raihanromzi <raihan.rakhman16@gmail.com>
2023-10-05 17:15:31 +0700

    Update tour-validation.js

--- src/validation/tour-validation.js
+++ src/validation/tour-validation.js
@@ -1,70 +1,96 @@
 import Joi from 'joi'
+import { errors } from '../utils/message-error.js'
 
 const addTourValidationSchema = Joi.object({
-    name: Joi.string().min(3).max(100).required().messages({
-        'string.base': 'name must be a string',
-        'string.min': 'min 3 characters',
-        'string.max': 'max 100 characters',
-        'any.required': 'name is required!',
-    }),
-    price: Joi.string().min(3).max(255).required().messages({
-        'string.base': 'price must be a string',
-        'string.min': 'min 3 characters',
-        'string.max': 'max 255 characters',
-        'any.required': 'price is required!',
-    }),
-    dateStart: Joi.date().required().messages({
-        'date.base': 'dateStart must be a date',
-        'any.required': 'dateStart is required!',
-    }),
-    dateEnd: Joi.date().required().messages({
-        'date.base': 'dateEnd must be a date',
-        'any.required': 'dateEnd is required!',
-    }),
-    description: Joi.string().min(3).max(255).messages({
-        'string.base': 'description must be a string',
-        'string.min': 'min 3 characters',
-        'string.max': 'max 255 characters',
-    }),
+    name: Joi.string()
+        .min(3)
+        .max(255)
+        .required()
+        .messages({
+            'string.base': `${errors.TOUR.NAME.MUST_STRING}`,
+            'string.empty': `${errors.TOUR.NAME.CANNOT_EMPTY}`,
+            'string.min': `${errors.TOUR.NAME.MUST_MIN}`,
+            'string.max': `${errors.TOUR.NAME.MUST_MAX}`,
+            'any.required': `${errors.TOUR.NAME.IS_REQUIRED}`,
+        }),
+    price: Joi.string()
+        .min(3)
+        .max(255)
+        .required()
+        .messages({
+            'string.base': `${errors.TOUR.PRICE.MUST_STRING}`,
+            'string.empty': `${errors.TOUR.PRICE.CANNOT_EMPTY}`,
+            'string.min': `${errors.TOUR.PRICE.MUST_MIN}`,
+            'string.max': `${errors.TOUR.PRICE.MUST_MAX}`,
+            'any.required': `${errors.TOUR.PRICE.IS_REQUIRED}`,
+        }),
+    dateStart: Joi.number()
+        .min(0)
+        .positive()
+        .required()
+        .messages({
+            'number.base': `${errors.TOUR.DATE_START.MUST_NUMBER}`,
+            'number.empty': `${errors.TOUR.DATE_START.CANNOT_EMPTY}`,
+            'number.min': `${errors.TOUR.DATE_START.MUST_MIN}`,
+            'number.positive': `${errors.TOUR.DATE_START.MUST_POSITIVE}`,
+            'any.required': `${errors.TOUR.DATE_START.IS_REQUIRED}`,
+        }),
+    dateEnd: Joi.number()
+        .min(0)
+        .positive()
+        .required()
+        .messages({
+            'number.base': `${errors.TOUR.DATE_END.MUST_NUMBER}`,
+            'number.empty': `${errors.TOUR.DATE_END.CANNOT_EMPTY}`,
+
+            'number.min': `${errors.TOUR.DATE_END.MUST_MIN}`,
+            'number.positive': `${errors.TOUR.DATE_END.MUST_POSITIVE}`,
+            'any.required': `${errors.TOUR.DATE_END.IS_REQUIRED}`,
+        }),
+    description: Joi.string()
+        .min(3)
+        .max(255)
+        .required()
+        .messages({
+            'string.base': `${errors.TOUR.DESCRIPTION.MUST_STRING}`,
+            'string.empty': `${errors.TOUR.DESCRIPTION.CANNOT_EMPTY}`,
+            'string.min': `${errors.TOUR.DESCRIPTION.MUST_MIN}`,
+            'string.max': `${errors.TOUR.DESCRIPTION.MUST_MAX}`,
+            'any.required': `${errors.TOUR.DESCRIPTION.IS_REQUIRED}`,
+        }),
+    place: Joi.array()
+        .items(
+            Joi.string()
+                .required()
+                .messages({
+                    'string.base': `${errors.TOUR.PLACE.NAME.MUST_STRING}`,
+                    'string.empty': `${errors.TOUR.PLACE.NAME.CANNOT_EMPTY}`,
+                    'string.min': `${errors.TOUR.PLACE.NAME.MUST_MIN}`,
+                })
+        )
+        .min(1)
+        .required()
+        .error(new Error(`${errors.TOUR.PLACE.IS_REQUIRED}`)),
 }).unknown(true)
 
-const updateTourValidationSchema = Joi.object({
-    name: Joi.string().min(3).max(100).optional().messages({
-        'string.base': 'name must be a string',
-        'string.min': 'min 3 characters',
-        'string.max': 'max 100 characters',
-    }),
-    price: Joi.string().min(3).max(255).optional().messages({
-        'string.base': 'price must be a string',
-        'string.min': 'min 3 characters',
-        'string.max': 'max 255 characters',
-    }),
-    dateStart: Joi.date().optional().messages({
-        'date.base': 'dateStart must be a date',
-    }),
-    dateEnd: Joi.date().optional().messages({
-        'date.base': 'dateEnd must be a date',
-    }),
-    description: Joi.string().min(3).max(255).optional().messages({
-        'string.base': 'description must be a string',
-        'string.min': 'min 3 characters',
-        'string.max': 'max 255 characters',
-    }),
-}).unknown(true)
+const imagesValidationSchema = Joi.array()
+    .items(
+        Joi.object()
+            .required()
+            .keys({
+                path: Joi.string()
+                    .required()
+                    .messages({
+                        'string.base': `${errors.TOUR.IMAGES.PATH_MUST_STRING}`,
+                        'string.empty': `${errors.TOUR.IMAGES.CANNOT_EMPTY}`,
+                        'any.required': `${errors.TOUR.IMAGES.IS_REQUIRED}`,
+                    }),
+            })
+            .unknown(true)
+            .error(new Error(`${errors.TOUR.IMAGES.IMAGE_ARRAY}`))
+    )
+    .min(1)
+    .required()
+    .error(new Error(`${errors.TOUR.IMAGES.MUST_VALID}`))
 
-const getTourValidationSchema = Joi.number().required().messages({
-    'number.base': 'id must be a number',
-    'any.required': 'id is required!',
-})
-
-const deleteTourValidationSchema = Joi.number().required().messages({
-    'number.base': 'id must be a number',
-    'any.required': 'id is required!',
-})
-
-export {
-    addTourValidationSchema,
-    updateTourValidationSchema,
-    getTourValidationSchema,
-    deleteTourValidationSchema,
-}
+export { addTourValidationSchema, imagesValidationSchema }
