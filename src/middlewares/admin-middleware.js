26a138bef590dd6850e3ac501e7d7dc4a9666395
raihanromzi <raihan.rakhman16@gmail.com>
2023-10-11 20:08:23 +0700

    fix bug error

--- src/middlewares/admin-middleware.js
+++ src/middlewares/admin-middleware.js
@@ -5,6 +5,19 @@ import { errors } from '../utils/message-error.js'
 const adminMiddleware = async (req, res, next) => {
     const user = req.user
 
+    if (!user) {
+        res.status(errors.HTTP.CODE.UNAUTHORIZED)
+            .send(
+                response.responseError(
+                    errors.HTTP.CODE.UNAUTHORIZED,
+                    errors.HTTP.STATUS.UNAUTHORIZED,
+                    errors.USER.NOT_FOUND
+                )
+            )
+            .end()
+        return
+    }
+
     // find role in database
     const role = await prismaClient.role.findUnique({
         where: {
