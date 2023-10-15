af40ff29f2dd5eda6cb73af262044422b82a1342
raihanromzi <raihan.rakhman16@gmail.com>
2023-10-15 16:50:12 +0700

    refactor public api

--- src/routes/public-route.js
+++ src/routes/public-route.js
@@ -3,9 +3,6 @@ import publicController from '../controllers/public-controller.js'
 
 const publicRouter = express.Router()
 
-// for testing only
-// publicRouter.post('/v1/users', userController.add)
-
 publicRouter.post('/v1/users/login', publicController.login)
 
 export { publicRouter }
