69f6c2155e78301770947459a6b19069a5fceabd
raihanromzi <raihan.rakhman16@gmail.com>
2023-10-16 05:55:31 +0700

    add error handling

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { logger } from './application/logging.js'
 import { web } from './application/web.js'
 
-web.listen(3000, () => {
-    logger.info('Server is running on port 3000')
+web.listen(3000, (err) => {
+    if (err) {
+        logger.error(err)
+    } else {
+        logger.info('Server is running on port 3000')
+    }
 })
