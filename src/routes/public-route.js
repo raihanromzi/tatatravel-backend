aabda778cddf7161b156c978b3729dfecf9831dd
raihanromzi <raihan.rakhman16@gmail.com>
2023-10-17 13:48:26 +0700

    add testing purposes

--- src/routes/public-route.js
+++ src/routes/public-route.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import publicController from '../controllers/public-controller.js'
+import adminController from '../controllers/admin-controller.js'
 
 const publicRouter = express.Router()
+
+// For Testing Only
+publicRouter.post('/v1/users', adminController.add)
 
 publicRouter.post('/v1/users/login', publicController.login)
 
